84ea792fa33a9f932d7ef0afa303db9bfedddba0
darpan-t007 <darpan.theng@learner.manipal.edu>
2023-03-04 17:52:10 +0530

    added description

--- initialize.py
+++ initialize.py
@@ -1,5 +1,51 @@
 """
-./initialize.py
+File: ./initialize.py
 
+Description: This function initializes all the aerodynamic and propulsion parameters 
+which has to be entered by the user. This includes initialization of all force, velocity,
+moment of inertia, displacement and stability related components required in a six degree 
+of freedom model. 
 
+Function Inputs: None
+
+Function Outputs: 
+                - acceleration --> acceleration of the rocket at any time interval
+                - prediction --> variable to store predicted apogee at different intervals of tim-
+                - u --> X-component of the velocity vector
+                - v --> Y-component of the velocity vector
+                - w --> Z-component of the velocity vector
+                - p --> Yaw rate of the rocket in rad/s
+                - q --> Pitch rate of the rocket in rad/s
+                - r --> Roll rate of the rocket in rad/s
+                - psi --> Yaw angle of rocket in degrees
+                - theta --> Pitch angle of rocket in degrees
+                - phi --> Roll angle of rocket in degrees
+                - Xe --> Displacement along X-direction at any interval in meters
+                - Ye --> Displacement along Y-direction at any interval in meters
+                - Ze --> Displacement along Z-direction at any interval in meters
+                - timer --> time at any given time step in seconds
+                - velx --> Velocity along X-direction
+                - dt --> time step between each iteration in seconds (default = 0.01s)
+                - counter1 --> variable to track the number of iterations
+                - temp --> temperature at any given interval
+                - CP --> coefficient of pressure 
+                - C_roll --> coefficient for roll
+                - Cn_yaw --> coefficient for yaw moment
+                - Cn_pitch --> coefficient for pitch moment
+                - Cn_alpha --> coefficient for angle of attack
+                - Cd --> drag coefficient
+                - Mass --> mass of the rocket
+                - CG --> centre of gravity
+                - Ixx --> moment of inertia with respect to X-axis
+                - Iyy --> moment of inertia with respect to Y-axis
+                - Stab_Cal --> stability caliber of the rocket
+                - flag --> flag variable 
+                - phase --> variable to keep track of phase of rocket (0 for ascent and 1 for parachute)
+                - mphase --> determines the phase of the motors
+                - state --> tracks the airbrakes state (2000 when deployed and 500 when deployed)
+                - delay --> delay due to electrical constraints in airbrakes
+                - delaytracker --> delay due to mechanical constraints in airbrakes
+                - turb_gen --> generates turbulance constant
+                - drift --> range calculated from the launch pad location to the landing location
+                - vtrajectory --> variable for plotting rocket trajectory during ascent phase
 """
