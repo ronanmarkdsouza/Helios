b23127b1043f2215cc6f309ffcd129ef249e062d
Ashwin <ashvinraj555@gmail.com>
2023-03-04 15:51:03 +0530

    Added Description

---
+++ density_and_temp.py
@@ -0,0 +1,15 @@
+"""
+File:   ./density_and_temp.py
+
+Description:
+        This function takes in the altitude (m) of the rocket at a given time interval and the temperature (K) of the outside at sea level 
+        to compute density and temperature at every given time step respectively.  
+
+Function Inputes:
+        y --> Altitude of the rocket at a given time step in meters
+        t1 --> Temperature of the outside at sea level in kelvin
+
+Function Outputs:
+        d --> Density of the air at a given altitude
+        t --> Temperature of the air at a given altitude
+"""
