4f512466bbc2784273ec4e2132c9d43712824a0a
ronanmarkdsouza <ronanmarkdsouza@gmail.com>
2023-03-05 06:58:17 +0530

    added propulsion parameters

--- propulsion_para.py
+++ propulsion_para.py
@@ -24,3 +24,31 @@ Function Outputs:
         Iy --> Moment of Inertia along Y
         currentthrust --> Thrust for the given timestep
 """
+from scipy.interpolate import interp1d
+def propulsion_para(timer, length, odia, wetmass, drymass, nozmass, nozlen, time, thrust):
+    if timer>time[-1]:
+        currentthrust = 0
+        mass = drymass+nozmass #total mass
+        Ix1 = 0 #MI of motor grain
+        Ix2 = drymass*(odia/2)**2 #MI of casing assuming it is a thin cylinder
+        Ix3 = 0.5*nozmass*0.25*(odia**2)
+        cg = (((mass-nozmass)*(length/2))+(nozmass*(length+(nozlen/2))))/mass
+        Iy1 = 0
+        Iy2 = (0.5*drymass*(odia/2)**2)+((1/12)*drymass*length**2)+(drymass*(cg-(length/2))**2)
+        Iy3 = ((1/12)*nozmass*(((3/4)*(odia**2))+nozlen**2))+(nozmass*(cg-(length+(nozlen/2)))**2)
+    else:
+        f = interp1d(time, thrust, kind='cubic', fill_value="extrapolate")
+        currentthrust = f(timer) 
+        burnrate = (wetmass-drymass)/(time[-1])
+        dm = burnrate*timer
+        mass = (wetmass-dm)+nozmass
+        Ix1 = 0.5*(mass-drymass-nozmass)*0.25*(odia**2)
+        Ix2= drymass*(odia/2)**2
+        Ix3= 0.5*nozmass*0.25*(odia**2)
+        cg= (((drymass)*(length/2))+((mass-drymass)*((length)/2))+(nozmass*(length+(nozlen/2))))/mass
+        Iy1= ((1/12)*(mass-drymass)*(((3/4)*(odia**2))+(length)**2))+((mass-drymass)*(cg-((length)/2))**2)
+        Iy2= (0.5*drymass*(odia/2)**2)+((1/12)*drymass*length**2)+(drymass*(cg-(length/2))**2)
+        Iy3=((1/12)*nozmass*(((3/4)*(odia**2))+nozlen**2))+(nozmass*(cg-(length+(nozlen/2)))**2)
+    Ix = Ix1+Ix2+Ix3
+    Iy = Iy1+Iy2+Iy3
+    return mass,cg,Ix,Iy,currentthrust
