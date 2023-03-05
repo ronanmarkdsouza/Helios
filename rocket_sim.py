d1aa886903c9715e4e7bec07268123c9fcd5375e
ronanmarkdsouza <ronanmarkdsouza.com>
2023-03-05 06:24:01 +0530

    changes made

--- rocket_sim.py
+++ rocket_sim.py
@@ -3,3 +3,14 @@ File: ./rocket_sim.py
 
 Description: This is the driver function for the simulator
 """
+import pandas as pd
+from initialize import rocket_params
+rocketSim = rocket_params()
+
+def rocket_sim(rocket):
+    prop_data = pd.read_excel(rocket.prop_data)
+    thrust = prop_data.loc[:,'thrust'].to_list()
+    time = prop_data.loc[:,'time'].to_list()
+    aero_data_wab = pd.read_excel(rocket.aero_data, sheet_name=0)
+    aero_data_ab = pd.read_excel(rocket.aero_data, sheet_name=1)
+    
