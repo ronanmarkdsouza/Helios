b7dc400c2a481efe99364464fddd35e7588b7344
darpan-t007 <darpan.theng@learner.manipal.edu>
2023-03-05 07:08:20 +0530

    new function created

--- wind_forces.py
+++ wind_forces.py
@@ -22,25 +22,19 @@ Function Output:
 import numpy as np
 from scipy.interpolate import interp1d
 
-class Cylinder:
-    def __init__(self, radius, length, center_of_mass):
-        self.radius = radius
-        self.length = length
-        self.center_of_mass = center_of_mass
-
-class Fin:
-    def __init__(self, dimensions, orientation):
-        self.dimensions = dimensions
-        self.orientation = orientation
-
-def calculate_side_forces(velocity, cylinder, fluid_density, fin):
-    velocity_magnitude = np.sqrt(velocity * velocity)
-
-    cdcyl = [[-5, 5], [10, 3.33], [100, 1.334], [1000, 0.738], [10000, 0.766], [100000, 0.56], [1000000, 0.55], [10000000, 0.54], [100000000, 0.54]]
-    reynolds_number = (fluid_density * velocity_magnitude**2 * cylinder.radius) / 0.0000173
-    cylinder_drag_coefficient = interp1d([data[0] for data in cdcyl], [data[1] for data in cdcyl], kind='linear')(reynolds_number)
-    cdplt = 1.28
-    F = 0.5 * fluid_density * velocity_magnitude**2 * ((cylinder_drag_coefficient * cylinder.radius * 2 * cylinder.length) * (cdplt * (fin.dimensions[0] + fin.dimensions[1]) * fin.dimensions[2]))
-    mom = 0.5 * fluid_density * velocity_magnitude**2 * cdplt * (fin.dimensions[0] + fin.dimensions[1]) * fin.dimensions[2] * (fin.orientation[1] - cylinder.center_of_mass)
-
-    return F, mom
+def side_forces1(vel,cm,den,rad,len,fin):
+    vel = np.sqrt(vel*vel)   
+    cdcyl=[[-5,5],[10 ,3.33],[100 ,1.334],[1000 ,0.738],[10000 ,0.766],[100000 ,0.56],[1000000 ,0.55],[10000000, 0.54],[100000000 ,0.54]]
+    cdcyl_1 = []
+    cdcyl_2 = []
+    for i in cdcyl:
+        cdcyl_1.append(i[0])
+        cdcyl_2.append(i[1])
+    cdplt=1.28;  
+    ryno=(1.225*vel**2*rad)/(0.0000173);    
+    f = interp1d(cdcyl_1,cdcyl_2, kind = 'linear')
+    cn = f(ryno)
+    F = 0.5* den* vel**2*((cn*rad*2*len)*(cdplt*(fin[0]+fin[1])*fin[2]))
+    mom = 0.5*den*vel**2*cdplt*(fin[0]+fin[1])*fin[2]*(fin[4]-cm)
+    
+    return F,mom
