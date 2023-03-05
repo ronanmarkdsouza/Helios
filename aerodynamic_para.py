38c68b06d217a474bcdd5fabd9ecd9efebf05769
Ashwin <ashvinraj555@gmail.com>
2023-03-05 06:35:48 +0530

    Initialised values

--- aerodynamic_para.py
+++ aerodynamic_para.py
@@ -21,3 +21,21 @@ Function Outputs:
                CN_alpha --> normal force coefficient with respect to angle of attack of the rocket
                CD --> drag coefficient at any time interval
 """
+import numpy as np
+from scipy.interpolate import interp1d
+
+def aerodynamic_para(data1, data2, mach, pitch, yaw, phase, flag):
+    m = np.arange(0,1.99,0.01)
+    cd_poweron = np.zeros((199,3,2))
+    cn = np.zeros((199,3))
+    cd_poweroff = np.zeros((199,3,2))
+    cn_alpha = np.zeros((199,1))
+    cp = np.zeros((199,1))
+    angle=[0,2,4]
+    mach = np.abs(mach)
+    pitch = np.abs(pitch)
+    yaw = np.abs(yaw)
+    
+#NO Airbrakes
+    
+    
